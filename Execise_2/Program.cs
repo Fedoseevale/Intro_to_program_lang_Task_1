46de787e3ba38021f6188735cf81c466be8b1d09
Александр <fedoseevale@gmail.com>
2022-09-22 22:14:56 +0400

    Execise_2 done

--- Execise_2/Program.cs
+++ Execise_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write ("Введите число A: ");
+// присваиваем переменным тип double, чтобы можно было работать с дробными числами
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите число B, отличное от А: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.Write ($"Большее число: {numberA}, меньшее число {numberB} ");
+}
+if (numberA < numberB)
+{
+    Console.Write ($"Большее число: {numberB}, меньшее число: {numberA} ");
+}
