049d6d9e2b003c196301057f45ffd47a32b8c3da
Александр <fedoseevale@gmail.com>
2022-09-22 23:57:25 +0400

    Execise_4 done

--- Execise_4/Program.cs
+++ Execise_4/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write ("Введите число A: ");
+// присваиваем переменным тип double, чтобы можно было работать с дробными числами
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите число B: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+Console.Write ("Введите число D: ");
+double numberD = Convert.ToDouble(Console.ReadLine());
+
+if ((numberA > numberB) && (numberA > numberD))
+{
+    Console.Write ($"Максимальное число: {numberA}");
+}
+if ((numberA == numberB) && (numberA > numberD))
+{
+    Console.Write ($"Максимальное число: {numberA}");
+}
+if ((numberA == numberD) && (numberA > numberB))
+{
+     Console.Write ($"Максимальное число: {numberA}");
+}
+if ((numberB > numberA) && (numberB > numberD))
+{
+     Console.Write ($"Максимальное число: {numberB}");
+}
+if ((numberD > numberA) && (numberD > numberB))
+{
+     Console.Write ($"Максимальное число: {numberD}");
+}
+if ((numberD == numberA) && (numberD > numberB))
+{
+     Console.Write ($"Максимальное число: {numberD}");
+}
+if ((numberD == numberB) && (numberD > numberA))
+{
+     Console.Write ($"Максимальное число: {numberD}");
+}
+if ((numberA == numberB) && (numberA == numberB))
+{
+     Console.Write ("Максимальное число отсутствует, все числа равны");
+}
